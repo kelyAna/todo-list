136afc37c6c266ad71da61989c13e42c3bb05e8b
kelyAna <kely.lopes@vtex.com.br>
2023-10-13 15:51:38 -0300

    feat: add todo-list project

---
+++ src/test/java/br/com/anakely/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.anakely.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
